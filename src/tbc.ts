55b02fa62f4770511801310a91b0402462423762
sunneydev <sunneyxdev@gmail.com>
2022-12-02 06:54:15 +0400

    remove idiot stuff

--- src/tbc.ts
+++ src/tbc.ts
@@ -7,31 +7,6 @@ export class TBC extends Auth {
     super();
   }
 
-  private async _withCredentials(opts?: AuthOptions): Promise<void> {
-    const { credentials, trustDevice, saveSession } = opts || {};
-
-    await this.authWithCredentials(credentials);
-
-    this._isLoggedIn = true;
-
-    if (trustDevice) await this._trustDevice();
-    if (saveSession) await this.saveSession();
-  }
-
-  private async _withSession() {
-    await this.withSession();
-    this._isLoggedIn = await this.checkLogin();
-
-    if (!this._isLoggedIn) {
-      throw new Error("Session expired or invalid");
-    }
-  }
-
-  public auth = {
-    withCredentials: this._withCredentials.bind(this),
-    withSession: () => this._withSession(),
-  };
-
   public async getUserInfo() {
     const { data } = await this._requests.get<UserInfo>("/user/v1/info");
 
@@ -49,3 +24,18 @@ export class TBC extends Auth {
     return data;
   }
 }
+
+const tbc = new TBC();
+
+const options = {
+  auth: {
+    withCredentials: async (opts?: AuthOptions) => {
+      await tbc.withCredentials(opts);
+    },
+    withSession: async () => {
+      await tbc.withSession();
+    },
+  },
+};
+
+export default options;
