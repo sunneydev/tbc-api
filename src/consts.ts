827ff0c897a8825f896edecfe271fd3c99b61070
sunneydev <sunneyxdev@gmail.com>
2022-12-02 06:53:57 +0400

    added defaults for fingerprint

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,5 @@
+import { Fingerprint } from "./types/tbc.types";
+
 export const keys = {
   sessionId: "JSESSIONID",
   restActionToken: {
@@ -16,3 +18,30 @@ export const TRUSTED_LOGIN_PAYLOAD = {
   businessObjectType: "3.58.01.00",
   type: "TrustedLoginDevice",
 } as const;
+
+export const defaultFingerprint: Fingerprint = {
+  browserFingerprint: 1678742652,
+  browserName: "Chrome",
+  browserVersion: "108.0.0.0",
+  operationSystemVersion: "10",
+  deviceTrackedData: JSON.stringify({
+    browser: "Chrome",
+    engine: "WebKit",
+    os: "Windows 10",
+    osVersion: "10",
+    cpu: "amd64",
+    screen: {
+      currentResolution: "1920x1080",
+      availableResolution: "1920x1032",
+      colorDepth: 24,
+    },
+    fonts:
+      "Arial Black, Arial, Bauhaus 93, Calibri Light, Calibri, Cambria Math, Cambria, Candara, Comic Sans MS, Consolas, Constantia, Corbel, Courier New, Ebrima, Franklin Gothic Heavy, Franklin Gothic Medium, Gabriola, Georgia, Impact, Lucida Console, Lucida Sans Unicod",
+    localStorage: true,
+    isSessionStorage: true,
+    timeZone: "Georgia Standard Time",
+    language: "en-US",
+    isCookie: true,
+  }),
+  os: "Windows",
+};
